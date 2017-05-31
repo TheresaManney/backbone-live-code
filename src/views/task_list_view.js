a1053abfcf2ac197652eeb5d61f486f2a17a21d8
Theresa Manney <theresa.m.17@gmail.com>
2017-05-31 13:47:44 -0700

    Added more logic to the task_list_view.ls file

--- src/views/task_list_view.js
+++ src/views/task_list_view.js
@@ -8,6 +8,21 @@ var TaskListView = Backbone.View.extend({
     this.template = params.template;
   },
   render: function() {
+    // Clear the todo items
+    this.$('.todo-items').empty();
+    // could be called 'self'...means the same as 'that'
+    //Saved the reference to 'this'
+    var that = this;
+    // Looped through the collection
+    this.model.each(function(task) {
+      // Created a new view for each task
+      var taskView = new TaskView({
+        model: task,
+        template: that.template
+      });
+      // rendered the view and appended it to 'todo-items'
+      that.$('.todo-items').append(taskView.render().el);
+    });
 
     // this = myView.render().el
     return this;
