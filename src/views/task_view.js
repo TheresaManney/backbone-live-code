0e580bc1764821490287b4cfd4307bf74b9fec33
Theresa Manney <theresa.m.17@gmail.com>
2017-05-31 10:13:18 -0700

    created a TaskView with an initialize, also exported TaskView

--- src/views/task_view.js
+++ src/views/task_view.js
@@ -4,3 +4,15 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import $ from 'jquery';
 import Task from '../models/task.js';
+
+var TaskView = Backbone.View.extend({
+  initialize: function(params) {
+    // Get it the template you want to use for this view
+    this.template = params.template;
+  },
+  render: function() {
+
+  }
+});
+
+export default TaskView;
